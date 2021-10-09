7924b7682b87f33343f153b6dc125829bec317e3
lion cho <chowin21@gmail.com>
2021-10-09 19:51:36 +0900

    fix(dashboard): Add dashboard page

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -9,49 +9,49 @@ const GappedFlex = styled(Flex)`
 `
 
 const BoxContainer = styled(Flex)`
-gap: 24px;
-width: 100%;
-flex-direction: column;
-${({ theme }) => theme.mediaQueries.md} {
-  flex-direction: row;
-}
+  gap: 24px;
+  width: 100%;
+  flex-direction: column;
+  ${({ theme }) => theme.mediaQueries.md} {
+    flex-direction: row;
+  }
 `
 const EarningBoxContainer = styled(Flex)`
-gap: 24px;
-width: 100%;
-margin-top:31px;
-flex-direction: column;
-${({ theme }) => theme.mediaQueries.md} {
-  flex-direction: row;
-}
+  gap: 24px;
+  width: 100%;
+  margin-top: 31px;
+  flex-direction: column;
+  ${({ theme }) => theme.mediaQueries.md} {
+    flex-direction: row;
+  }
 `
 const StyledHeadingFlex = styled(Box)`
   display: flex;
   flex-direction: column;
   position: relative;
   padding-top: 156px;
-  width:400px;
+  width: 400px;
 `
 
 const PageHeading = styled(Box)`
   display: flex;
   align-self: baseline;
   position: relative;
-  color:#3763A4;
-  font-size:20px;
-  line-height:36px;
-  font-weight:bold;
+  color: #3763a4;
+  font-size: 20px;
+  line-height: 36px;
+  font-weight: bold;
 `
 const PageHeadingDesc = styled(Box)`
   display: flex;
   align-self: baseline;
   position: relative;
-  color:#99A2AB;
-  font-size:13px;
-  line-height:24px;
+  color: #99a2ab;
+  font-size: 13px;
+  line-height: 24px;
 `
 const StyledBoxCard = styled(Box)`
-flex-direction: column;
+  flex-direction: column;
   display: flex;
   align-self: baseline;
   position: relative;
@@ -63,91 +63,102 @@ const BoxCardInner = styled(Box)`
   padding: 20px 24px;
   background: ${({ theme }) => theme.card.background};
   border-radius: 4px;
-  margin-top:9px;
+  margin-top: 9px;
   box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.08);
 `
-const BalanceBox=styled(Box)`
-position:absolute;
-flex-direction: row;
-bottom:20px;
-right:24px;
-text-alight:right;
-justifyContent: flex-end;
+const BalanceBox = styled(Box)`
+  position: absolute;
+  flex-direction: row;
+  bottom: 20px;
+  right: 24px;
+  text-alight: right;
+  justifycontent: flex-end;
 `
 
-const BalanceText=styled(Box)`
-color:#3763A4;
-font-weight:bold;
-font-size:20px;
-display: inline-block;
-margin-left:2px;
+const BalanceText = styled(Box)`
+  color: #3763a4;
+  font-weight: bold;
+  font-size: 20px;
+  display: inline-block;
+  margin-left: 2px;
 `
-const BalanceCurrencyText=styled(Box)`
-color:#CAD0D7;
-font-weight:bold;
-font-size:20px;
-display: inline-block;
+const BalanceCurrencyText = styled(Box)`
+  color: #cad0d7;
+  font-weight: bold;
+  font-size: 20px;
+  display: inline-block;
 `
-const EarningTitle=styled(Box) `
-color:#FF0000;
-font-size:16px;
-position:absolute;
-left:16px;
-`
-
-const EarningUpperBox=styled(Box)`
-border-bottom:1px solid #DEE2E6;
-padding:16px 0;
-flexDirection:row;
-`
-const EarningLowerBox=styled(Box)`
-padding:16px 0 0 0;
-flexDirection:row;
-`
-const EarningUpperLeftBox=styled(Box)`
-border-right:1px solid #DEE2E6;
-padding:0 24px;
-width:50%;
-display:inline-block;
-font-family: Roboto;
-`
-const EarningUpperRightBox=styled(Box)`
-padding:0 24px;
-width:50%;
-display:inline-block;
-`
-const EarningBalanceHeading=styled(Box)`
-color:#99A2AB;
-font-size:13px;
-font-family: Roboto;
-`
-const EarningBalanceQty=styled(Box)`
-color:#4D5560;
-font-size:20px;
-text-align:right;
-font-family: Roboto;
-`
-const EarningBalanceAmt=styled(Box)`
-color:#DEE2E6;
-font-size:12px;
-text-align:right;
-font-family: Roboto;
+const EarningTitle = styled(Box)`
+  color: #ff0000;
+  font-size: 16px;
+  position: absolute;
+  left: 16px;
 `
 
-type Step = {cardno:number;heading:string;  title: string;currencysymbol: string; balance: string }
-type Earning = {title:string;heading:string;dailyTx: number; dailyAmt: number;weeklyTx: number; weeklyAmt: number;monthlyTx: number; monthlyAmt: number;yearlyTx: number; yearlyAmt: number }
+const EarningUpperBox = styled(Box)`
+  border-bottom: 1px solid #dee2e6;
+  padding: 16px 0;
+  flexdirection: row;
+`
+const EarningLowerBox = styled(Box)`
+  padding: 16px 0 0 0;
+  flexdirection: row;
+`
+const EarningUpperLeftBox = styled(Box)`
+  border-right: 1px solid #dee2e6;
+  padding: 0 24px;
+  width: 50%;
+  display: inline-block;
+  font-family: Roboto;
+`
+const EarningUpperRightBox = styled(Box)`
+  padding: 0 24px;
+  width: 50%;
+  display: inline-block;
+`
+const EarningBalanceHeading = styled(Box)`
+  color: #99a2ab;
+  font-size: 13px;
+  font-family: Roboto;
+`
+const EarningBalanceQty = styled(Box)`
+  color: #4d5560;
+  font-size: 20px;
+  text-align: right;
+  font-family: Roboto;
+`
+const EarningBalanceAmt = styled(Box)`
+  color: #dee2e6;
+  font-size: 12px;
+  text-align: right;
+  font-family: Roboto;
+`
+
+type Step = { cardno: number; heading: string; title: string; currencysymbol: string; balance: string }
+type Earning = {
+  title: string
+  heading: string
+  dailyTx: number
+  dailyAmt: number
+  weeklyTx: number
+  weeklyAmt: number
+  monthlyTx: number
+  monthlyAmt: number
+  yearlyTx: number
+  yearlyAmt: number
+}
 
 const BoxCard: React.FC<{ step: Step }> = ({ step }) => {
   return (
     <StyledBoxCard width="100%">
-      <Text color="#FF0000">{step.heading}</Text>
       <BoxCardInner height={['173px']}>
-        <Text color="#4D5560" fontSize="13">{step.title}</Text>
-        <BalanceBox >
+        <Text color="#4D5560" fontSize="13">
+          {step.title}
+        </Text>
+        <BalanceBox>
           <BalanceCurrencyText>{step.currencysymbol}</BalanceCurrencyText>
           <BalanceText>{step.balance}</BalanceText>
         </BalanceBox>
-        
       </BoxCardInner>
     </StyledBoxCard>
   )
@@ -156,35 +167,35 @@ const BoxCard: React.FC<{ step: Step }> = ({ step }) => {
 const EarningCard: React.FC<{ earning: Earning }> = ({ earning }) => {
   return (
     <StyledBoxCard width="100%">
-      <EarningTitle>{earning.title}</EarningTitle>
       <BoxCardInner>
-        <Text color="#4D5560" fontSize="13">{earning.heading}</Text>
+        <Text color="#4D5560" fontSize="13">
+          {earning.heading}
+        </Text>
         <EarningUpperBox>
-          <EarningUpperLeftBox> 
+          <EarningUpperLeftBox>
             <EarningBalanceHeading>Daily</EarningBalanceHeading>
             <EarningBalanceQty>{earning.dailyTx}</EarningBalanceQty>
             <EarningBalanceAmt>($ {earning.dailyAmt})</EarningBalanceAmt>
-            </EarningUpperLeftBox>
+          </EarningUpperLeftBox>
           <EarningUpperRightBox>
-          <EarningBalanceHeading>Weekly</EarningBalanceHeading>
+            <EarningBalanceHeading>Weekly</EarningBalanceHeading>
             <EarningBalanceQty>{earning.weeklyTx}</EarningBalanceQty>
             <EarningBalanceAmt>($ {earning.weeklyAmt})</EarningBalanceAmt>
           </EarningUpperRightBox>
         </EarningUpperBox>
-        
+
         <EarningLowerBox>
-        <EarningUpperLeftBox> 
+          <EarningUpperLeftBox>
             <EarningBalanceHeading>Monthly</EarningBalanceHeading>
             <EarningBalanceQty>{earning.monthlyTx}</EarningBalanceQty>
             <EarningBalanceAmt>($ {earning.weeklyAmt})</EarningBalanceAmt>
-            </EarningUpperLeftBox>
+          </EarningUpperLeftBox>
           <EarningUpperRightBox>
-          <EarningBalanceHeading>Yearly</EarningBalanceHeading>
+            <EarningBalanceHeading>Yearly</EarningBalanceHeading>
             <EarningBalanceQty>{earning.yearlyTx}</EarningBalanceQty>
             <EarningBalanceAmt>($ {earning.yearlyAmt})</EarningBalanceAmt>
           </EarningUpperRightBox>
         </EarningLowerBox>
-        
       </BoxCardInner>
     </StyledBoxCard>
   )
@@ -194,23 +205,23 @@ const Dashboard = () => {
   const { t } = useTranslation()
   const steps: Step[] = [
     {
-      cardno:1,
+      cardno: 1,
       heading: t('TVL all pools'),
-      title: t('Assets in Deposit'),
+      title: t('TVL all pools'),
       currencysymbol: t('$'),
       balance: t('0.00'),
     },
     {
-      cardno:2,
+      cardno: 2,
       heading: t('Dooda Holdingds'),
-      title: t('DOODA Possession'),
+      title: t('Dooda Holdingds'),
       currencysymbol: t(''),
       balance: t('0.00'),
     },
     {
-      cardno:3,
+      cardno: 3,
       heading: t('Dooda price'),
-      title: t('DOODA Current Price'),
+      title: t('Dooda price'),
       currencysymbol: t('$'),
       balance: t('3.52'),
     },
@@ -218,28 +229,28 @@ const Dashboard = () => {
 
   const earnings: Earning[] = [
     {
-      title:t('your earnings'),
-      heading: t('Compensation by period($)'),
+      title: t('your earnings'),
+      heading: t('Your earnings'),
       dailyTx: 0,
-      dailyAmt: 0.00,
+      dailyAmt: 0.0,
       weeklyTx: 0,
-      weeklyAmt: 0.00,
+      weeklyAmt: 0.0,
       monthlyTx: 0,
-      monthlyAmt: 0.00,
+      monthlyAmt: 0.0,
       yearlyTx: 0,
-      yearlyAmt: 0.00,
+      yearlyAmt: 0.0,
     },
     {
-      title:t('your APR'),
-      heading: t('expected return($)'),
+      title: t('your APR'),
+      heading: t('Your APR'),
       dailyTx: 0,
-      dailyAmt: 0.00,
+      dailyAmt: 0.0,
       weeklyTx: 0,
-      weeklyAmt: 0.00,
+      weeklyAmt: 0.0,
       monthlyTx: 0,
-      monthlyAmt: 0.00,
+      monthlyAmt: 0.0,
       yearlyTx: 0,
-      yearlyAmt: 0.00,
+      yearlyAmt: 0.0,
     },
   ]
 
@@ -247,37 +258,33 @@ const Dashboard = () => {
     <Page>
       <GappedFlex flexDirection={['column', 'column', 'column', 'row']}>
         <Flex flex="2" flexDirection="column">
-<StyledHeadingFlex>
-<PageHeading>
-          {t('My Asset Report')}
-        </PageHeading>
-        <PageHeadingDesc>{t('Make a deposit and earn money. Opportunities for steady revenue generation are at hand.')}</PageHeadingDesc>
-</StyledHeadingFlex>
-        
+          <StyledHeadingFlex>
+            <PageHeading>{t('My Asset Report')}</PageHeading>
+            <PageHeadingDesc>
+              {t('Make a deposit and earn money. Opportunities for steady revenue generation are at hand.')}
+            </PageHeadingDesc>
+          </StyledHeadingFlex>
         </Flex>
         <Flex flex="1" justifyContent="center">
-        <img src="/images/frame.png" width="336px" height="287px" alt="" />
+          <img src="/images/dashboard/frame.png" width="336px" height="287px" alt="" />
         </Flex>
       </GappedFlex>
 
-      
       <BoxContainer>
         {steps.map((step) => (
           <BoxCard key={step.cardno} step={step} />
         ))}
       </BoxContainer>
-      
+
       <GappedFlex flexDirection={['column', 'column', 'column', 'row']}>
-      <EarningBoxContainer>
-      {earnings.map((earning) => (
-          <EarningCard key={earning.title} earning={earning} />
-        ))}
-      </EarningBoxContainer>
+        <EarningBoxContainer>
+          {earnings.map((earning) => (
+            <EarningCard key={earning.title} earning={earning} />
+          ))}
+        </EarningBoxContainer>
       </GappedFlex>
     </Page>
   )
 }
 
 export default Dashboard
-
-
